c06a4fb30b00760743cbd1746a26ec30760db948
rabubu <88380535+rabubu@users.noreply.github.com>
2023-07-02 20:25:16 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,6 @@
- <?php
+<?php
 
+  // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'sweatstellar@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
